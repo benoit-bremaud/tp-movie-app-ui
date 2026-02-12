01c1a2c56e3991ee448901ca47db0224c2eb6c1e
Benoît Bremaud <benoit.bremaud@vev.com>
2026-02-12 14:31:02 +0100

    chore(project): remove unused web vitals bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,14 @@
 // This must be imported for first
 import "reflect-metadata";
-
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
 // enables i18n
 import "./i18n";
 
-import { container } from "tsyringe";
+import App from "./App";
 import { EnvService } from "./services/envService";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { container } from "tsyringe";
 
 container.resolve(EnvService);
 
@@ -23,8 +20,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
